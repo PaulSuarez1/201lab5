77b0107dfe13c37cb340de1eb6f409c8ed464c7d
Paul Suarez <paulsblogging.com@gmail.com>
2018-05-04 18:27:05 -0700

    solved problem-5

--- app.js
+++ app.js
@@ -98,17 +98,23 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 
 "The numbers 2,3,4 have a product of 24."
 
+
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+var testMultArray = [2, 3, 4];
 
+function multiplyArray(testMultArray) { //eslint-disable-line
+    var multArray = multiply(testMultArray[0], testMultArray[1])[0];
+    var multArrayAll = multiply(multArray, testMultArray[2])[0];
+    var messageMultArray = 'The numbers ' + testMultArray[0] + ',' + testMultArray[1] + ',' + testMultArray[2] + ' have a product of ' + multArrayAll + '.';
+    return [multArrayAll, messageMultArray];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
